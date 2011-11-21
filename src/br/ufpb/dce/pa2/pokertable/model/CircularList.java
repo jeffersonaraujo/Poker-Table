f029adb8ced9ba8f97bd2ea86c4d0f282212c583
Vicente Neto <vicente.neto@dce.ufpb.br>
2011-11-21 17:29:33 -0200

    Atualizando lista circular

--- src/br/ufpb/dce/pa2/pokertable/model/CircularList.java
+++ src/br/ufpb/dce/pa2/pokertable/model/CircularList.java
@@ -1,100 +1,64 @@
 package br.ufpb.dce.pa2.pokertable.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-public class CircularList<E> extends ArrayList<E> {
-	private int actualIndex;
-
+public class CircularList{
+	private Player[] lista;
+	private int size;
+	
 	public CircularList() {
-		super();
+		this.lista = new Player[4];
+		this.size = 0;
 	}
-	public CircularList(final Collection<E> collection) {
-		super(collection);
-	}
-
-	@Override
-	public E get(int index) {
-		index = index % this.size();
-		return super.get(index);
-	}
-	@Override
-	public void add(final int index, final E element) {
-		if (index <= this.actualIndex)
-			this.actualIndex++;
-
-		super.add(index, element);
-	};
-	@Override
-	public boolean addAll(int index, Collection<? extends E> collection) {
-		if (index <= this.actualIndex)
-			this.actualIndex = this.actualIndex + collection.size();
-
-		return super.addAll(index, collection);
-	}
-	@Override
-	public E remove(final int index) {
-		final E element = super.remove(index);
-
-		if (index == actualIndex)
-			this.actualIndex = 0;
-		else if (index < this.actualIndex)
-			this.actualIndex--;
-
-		return element;
-	}
-	@Override
-	public boolean remove(final Object object) {
-		final int index = this.indexOf(object);
-
-		return this.remove(index) != null;
-	}
-	@Override
-	public boolean removeAll(final Collection<?> collection) {
-		int nextIndex;
-
-		if (collection.contains(this.getActual()))
-			nextIndex = 0;
-		else {
-			int discount = 0;
-			for (Object object : collection)
-				if (this.contains(object)) {
-					final int objectIndex = this.indexOf(object);
-					if (objectIndex < this.actualIndex)
-						discount++;
-				}
-			nextIndex = this.actualIndex - discount;
+	
+	public boolean add(Player o){
+		for(int i = 0; i < lista.length; i++){
+			if(lista[i].equals(null)){
+				lista[i] = o;
+				this.size++;
+				return true;
+			}
 		}
-
-		final boolean result = super.removeAll(collection);
-
-		if (result)
-			this.actualIndex = nextIndex;
-
-		return result;
+		return false;
 	}
-	@Override
-	public void clear() {
-		this.actualIndex = 0;
-		super.clear();
+	
+	public boolean remove(Player o){
+		for(int i = 0; i < lista.length; i++)
+			if(lista[i].equals(o)){
+				lista[i] = null;
+				this.size--;
+				return true;
+			}
+		return false;
 	}
-	public E previous() {
-		if (this.actualIndex == 0)
-			this.actualIndex = this.size() - 1;
+	
+	public boolean remove(int i){
+		if(lista[i] != null){
+			lista[i] = null;
+			this.size--;
+			return true;
+		}
+		return false;
+	}
+	
+	public Player get(int i){
+		return lista[i];
+	}
+	
+	public Player next(int i){
+		if(i < lista.length - 1)
+			return lista[i + 1];
 		else
-			this.actualIndex--;
-
-		return super.get(actualIndex);
+			return lista[0];
 	}
-	public E getActual() {
-		return super.get(actualIndex);
+	
+	public int size(){
+		return this.size;
 	}
-	public E next() {
-		if (this.actualIndex == this.size() - 1)
-			this.actualIndex = 0;
-		else
-			this.actualIndex++;
-
-		return super.get(actualIndex);
+	
+	public String toString(){
+		String string = "";
+		for(int i = 0; i < lista.length; i++)
+			if(!lista[i].equals(null))
+				string += "Nome: " + lista[i].getName() + "Posição: " + i + "\n";
+		return string;
 	}
 }
