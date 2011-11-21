90c834d361f7d0deb63cc8e2cac1c5c4edfb9914
Vicente Neto <vicente.neto@dce.ufpb.br>
2011-11-21 14:59:25 -0200

    Alteração da classe Player

--- src/br/ufpb/dce/pa2/pokertable/model/Player.java
+++ src/br/ufpb/dce/pa2/pokertable/model/Player.java
@@ -1,5 +1,53 @@
 package br.ufpb.dce.pa2.pokertable.model;
 
-public class Player {
+public class Player{
+	private String name;	
+	private int age;
+	private int money;
+	private int points;
+		
+	public Player(){
+		this("Player",0,0);
+		this.points = 0;
+	}
 	
+	public Player(String nome){
+		this(nome, 0, 0);
+		this.points = 0;
+	}
+	
+	public Player(String name, int age, int money) {
+		this.name = name;
+		this.age = age;
+		this.money = money;
+		this.points = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getPoints() {
+		return this.points;
+	}
+	
+	public int getMoney() {		
+		return this.money;
+	}
+	
+	public void setMoney(int money) {
+		this.money = money;
+	}	
 }
